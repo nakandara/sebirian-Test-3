71a67c94f3391979e0cc0427681a40cc0e3a59b6
PRAMOD <pramodnakandara@gmail.com>
2023-03-19 12:40:15 +0530

    customize border

--- src/app/dashboard/Home/AppBar.js
+++ src/app/dashboard/Home/AppBar.js
@@ -18,8 +18,8 @@ const Item = styled(Paper)(({ theme }) => ({
 export default function AppBar() {
     return (
         <Box className="drop-shadow-2xl" sx={{ flexGrow: 1 }}>
-            <div className="grid grid-cols-4 gap-4 font-lora drop-shadow-2xl ml-8 w-full p-2 justify-center ">
-                <div className="sm:flex  md:flex space-x-5 border-r border-blue-900">
+            <div className="grid grid-cols-4 gap-4 font-lora drop-shadow-2xl  justify-center ">
+                <div className="sm:flex  md:flex space-x-5 border-r ml-8 w-full p-2 border-blue-900">
                     <div className="hover:text-green-400">
                         <div>
                             <Link to="/primaryAcounts">Primarry Account</Link>
@@ -28,14 +28,14 @@ export default function AppBar() {
                     </div>
                 </div>
 
-                <div className="sm:flex  md:flex space-x-5  border-r border-blue-900">
+                <div className="sm:flex  md:flex space-x-5  border-r border-blue-900 ml-8 w-full p-2">
                     <div> </div>
                     <div className="hover:text-green-400">
                         <Link to="/Actions">Taxi to Roskider</Link>
                         <div className="font-bold">-27088.2242 Kr</div>
                     </div>
                 </div>
-                <div className="flex space-x-4 border-r border-blue-900">
+                <div className="flex space-x-4 border-r border-blue-900 ml-8 w-full p-2">
                     <div> </div>
                     <div className="hover:text-green-400">
                         <Link to="/Actions">Peter Wilgard</Link>
